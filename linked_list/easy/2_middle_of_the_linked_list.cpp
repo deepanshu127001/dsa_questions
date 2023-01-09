448bb2cc7a5e20bf61712e6e29d0723186e87247
deepanshu127001 <deepanshu127001@gmail.com>
2023-01-09 22:55:23 +0530

    middle of the linked list

---
+++ linked_list/easy/2_middle_of_the_linked_list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+struct ListNode {
+    int data;
+    ListNode *next;
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow =  head;
+        ListNode *fast = head->next;
+        while(fast!=NULL){
+            fast = fast ->next;
+            if(fast!=NULL)
+                fast = fast-> next;
+            slow = slow->next;
+        }
+        return slow ;
+    }
+};
