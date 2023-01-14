2f86ed44de6044c2b6105027203411d70bc95736
deepanshu127001 <deepanshu127001@gmail.com>
2023-01-14 19:19:46 +0530

    leetcode 706 optimized

--- linked_list/easy/7_design_on_hash_map.cpp
+++ linked_list/easy/7_design_on_hash_map.cpp
@@ -1,23 +1,85 @@
 #include <vector>
-
+#include <utility>
+#include <list>
+using namespace std;
+//optimized (chaining)
 class MyHashMap {
 public:
-std::vector<int> vec;
+vector<list<pair<int,int>>> vec;
+int size;
     MyHashMap() {
-        int size = 1e6+1;
+         size=50;
         vec.resize(size);
-        fill(vec.begin(),vec.end(),-1);
     }
-    
+    int getHash(int key){
+        return key%size;
+    }
+    list<pair<int,int>>::iterator search(int key){
+        int idx = getHash(key);
+        list<pair<int,int>>::iterator iter = vec[idx].begin();
+        while(iter!=vec[idx].end()){
+            if(iter->first==key){
+                return iter;
+            }
+            iter++;
+        }
+        return vec[idx].end();
+    }
     void put(int key, int value) {
-        vec[key]=value;
+        int idx = getHash(key);
+        remove(key);
+        vec[idx].push_back({key,value});
     }
     
     int get(int key) {
-        return vec[key];
+        int idx =  getHash(key);
+        list<pair<int,int>>::iterator iter = search(key);
+        
+        if(iter==vec[idx].end()){
+            return -1;
+        }
+        else{
+            return iter->second;
+        }
+
     }
     
     void remove(int key) {
-        vec[key]=-1;
+        int idx =  getHash(key);
+        list<pair<int,int>>::iterator iter=search(key);
+        if(iter!=vec[idx].end())
+        vec[idx].erase(search(key));
+
     }
 };
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+
+//bruteforce
+// class MyHashMap {
+// public:
+// std::vector<int> vec;
+//     MyHashMap() {
+//         int size = 1e6+1;
+//         vec.resize(size);
+//         fill(vec.begin(),vec.end(),-1);
+//     }
+    
+//     void put(int key, int value) {
+//         vec[key]=value;
+//     }
+    
+//     int get(int key) {
+//         return vec[key];
+//     }
+    
+//     void remove(int key) {
+//         vec[key]=-1;
+//     }
+// };
