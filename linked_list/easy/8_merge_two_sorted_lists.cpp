038e0e4d88a433282588a2c1009d4a733f5f3bf6
deepanshu127001 <deepanshu127001@gmail.com>
2023-01-16 09:17:42 +0530

    leecode 21 iterative approach

--- linked_list/easy/8_merge_two_sorted_lists.cpp
+++ linked_list/easy/8_merge_two_sorted_lists.cpp
@@ -11,6 +11,38 @@ struct ListNode {
 
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)
+            return list2;
+        if(list2==NULL)
+            return list1;
+        ListNode *ans = new ListNode(-1);
+        ListNode *tail = ans;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                tail->next = list1;
+                tail = list1;
+                list1=list1->next;
+                
+            }
+            else{
+                tail->next = list2;
+                tail = list2;
+                list2=list2->next;
+            }
+        }
+        if(list1==NULL){
+            tail->next=list2;
+        }
+        else{
+            tail->next = list1;
+        }
+        return ans->next;
+    }
+};
+/*
+class Solution {
+public:
     ListNode* merge(ListNode *l1,ListNode *l2){
         if(l1==NULL)
             return l2;
@@ -29,3 +61,4 @@ public:
         return merge(list1,list2);
     }
 };
+*/
