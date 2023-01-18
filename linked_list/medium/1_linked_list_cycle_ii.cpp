69a5771ad854cca5df6c048674a47a522fd37463
deepanshu127001 <deepanshu127001@gmail.com>
2023-01-18 23:51:29 +0530

    leetcode 142

---
+++ linked_list/medium/1_linked_list_cycle_ii.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !head->next) return nullptr;
+
+        ListNode *slow = head->next, *fast = head -> next-> next;
+        int answer = 0;
+
+        while(fast != slow) {
+            slow = slow -> next;
+            if(fast == nullptr || fast -> next == nullptr) return nullptr;
+            fast = fast -> next -> next;
+        }
+
+        slow = head;
+        while(slow != fast) {
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+
+        return fast;
+    }
+};
